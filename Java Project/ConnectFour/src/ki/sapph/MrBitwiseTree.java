6cda82e82d3d4eee9e6cfed28e4683a2ccc8e9e0
SimonBlasen <simon.yzf@t-online.de>
2017-12-25 00:27:11 +0100

    Bit Geschissel

---
+++ Java Project/ConnectFour/src/ki/sapph/MrBitwiseTree.java
@@ -0,0 +1,495 @@
+package ki.sapph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import de.cogsys.ai.sogo.control.SogoGameConsole;
+import de.cogsys.ai.sogo.game.SogoGame;
+import de.cogsys.ai.sogo.game.SogoMove;
+import de.cogsys.ai.sogo.game.SogoGame.Player;
+import de.cogsys.ai.sogo.player.SogoPlayer;
+import util.GameAnalyzer;
+
+public class MrBitwiseTree implements SogoPlayer {
+
+	private Player p;
+	private SogoGameConsole c;
+
+	private boolean doneFirstMove = false;
+	
+	private static final double win_p = 1000;
+	private static final double triple_p = 20;
+	private static final double double_p = 2;
+	private static final double single_p = 1;
+	
+	
+	private List<Integer> bestIndices = new ArrayList<Integer>();
+	
+	
+	@Override
+	public void initialize(Player p) {
+		this.p = p;
+	}
+
+	@Override
+	public void generateNextMove(SogoGameConsole c)
+	{
+		this.c = c;
+		final SogoGame g = c.getGame();
+		final List<SogoMove> moves = g.generateValidMoves();
+		
+		double[] currentValues = new double[16];
+		double[] nextValues = new double[16];
+		
+		c.updateMove(moves.get((new Random(0)).nextInt(moves.size())));
+		
+		
+		bestIndices.clear();
+		
+		if (doneFirstMove  || true)
+		{
+			int tookIndex = 0;
+			
+			double maxYet = -10000.0;
+			
+			SogoMove nextMove = moves.get(0);
+			
+			for (int depth = 5; depth <= 20; depth += 1)
+			{
+				boolean[][][] b1s = GameAnalyzer.getB1sFromGame(g);
+				boolean[][][] b2s = GameAnalyzer.getB2sFromGame(g);
+				
+				double val = evaluateNode(b1s, b2s, true, -1000000.0, 1000000.0, depth, true, 0);
+				
+
+				if (c.getTimeLeft() <= 0)
+				{
+					break;
+				}
+				int took = (new Random(0)).nextInt(bestIndices.size());
+				c.updateMove(new SogoMove(bestIndices.get(took) % 4, bestIndices.get(took) / 4));
+				
+				System.out.println("Depth [" + depth + "] done. Actions: " + bestIndices.size());
+				
+				System.out.println("Took value: " + bestIndices.get(took));
+				
+				
+			}
+			
+		}
+		else
+		{
+			doneFirstMove = true;
+		}
+		
+		
+	}
+	
+	private double evaluateNode(long bp1, long bp2, boolean isMax, double alpha, double beta, int depth, boolean firstNode, int moveIndex)
+	{
+		// Execute move at moveIndex
+		
+		long placedField = 0x0;
+		
+		if (firstNode == false)
+		{
+			for (int z = 0; z < 4; z++)
+			{
+				placedField = (0x00000001 << (moveIndex + z * 16));
+				
+				if ((bp1 & placedField) == 0x0 && (bp2 & placedField) == 0x0)
+				{
+					if (isMax)
+					{
+						bp1 |= placedField;
+					}
+					else
+					{
+						bp2 |= placedField;
+					}
+					break;
+				}
+			}
+		}
+		
+		
+
+		double bestValue = isMax ? -10000.0 : 10000.0;
+		if (c.getTimeLeft() <= 0)
+		{
+			return -10000.0;
+		}
+		
+		
+
+		if (depth <= 0 || GameAnalyzer.hasGameEnded(isMax ? bp1 : bp2))
+		{
+			double val = evaluateGame(bp1, bp2);
+			return val;
+		}
+		else
+		{
+			for (int i = 0; i < 16; i++)
+			{
+				if (bsp1[i % 4][i / 4][3] == false && c.getTimeLeft() > 0)
+				{
+					double childValue = evaluateNode(bsp1, bsp2, !isMax, alpha, beta, depth - 1, false, i);
+					
+					if (firstNode)
+					{
+						if (depth >= 7)
+						System.out.println("Move [" + i + "] is " + childValue);
+					}
+					
+					if (childValue > bestValue == isMax)
+					{
+						if (firstNode)
+						{
+							bestIndices.clear();
+							bestIndices.add(i);
+						}
+						bestValue = childValue;
+					}
+					else if (childValue == bestValue)
+					{
+						if (firstNode)
+						{
+							//bestIndices.add(i);
+						}
+					}
+					if (isMax == false && childValue < beta)
+					{
+						beta = childValue;
+					}
+					else if (isMax && childValue > alpha)
+					{
+						alpha = childValue;
+					}
+					
+					//node.updateValue(child.getValue());
+					
+					if (alpha >= beta && firstNode == false)
+					{
+						// Pruning
+						
+						if (firstNode == false)
+						{
+							bsp1[x][y][z] = false;
+						}
+						return isMax ? 10000.0 : -10000.0;
+					}
+				}
+			}
+			
+			if (firstNode == false)
+			{
+				bsp1[x][y][z] = false;
+			}
+			return bestValue;
+		}
+		
+	}
+	
+	private double evaluateNode(boolean[][][] bsp1, boolean[][][] bsp2, boolean isMax, double alpha, double beta, int depth, boolean firstNode, int moveIndex)
+	{
+		int x = moveIndex % 4;
+		int y = moveIndex / 4;
+		int z = 0;
+		
+		if (firstNode == false)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				if (bsp1[x][y][i] == false)
+				{
+					z = i;
+					bsp1[x][y][i] = true;
+					bsp2[x][y][i] = isMax;
+					break;
+				}
+			}
+		}
+		
+		double bestValue = isMax ? -10000.0 : 10000.0;
+		
+		
+		if (c.getTimeLeft() <= 0)
+		{
+			if (firstNode == false)
+			{
+				bsp1[x][y][z] = false;
+			}
+			
+			return -10000.0;
+		}
+		if (depth <= 0 || GameAnalyzer.hasGameEnded(bsp1, bsp2, x, y, z, isMax))
+		{
+			double val = evaluateGame(bsp1, bsp2);
+			if (firstNode == false)
+			{
+				bsp1[x][y][z] = false;
+			}
+			return val;
+		}
+		else
+		{
+			for (int i = 0; i < 16; i++)
+			{
+				if (bsp1[i % 4][i / 4][3] == false && c.getTimeLeft() > 0)
+				{
+					double childValue = evaluateNode(bsp1, bsp2, !isMax, alpha, beta, depth - 1, false, i);
+					
+					if (firstNode)
+					{
+						if (depth >= 7)
+						System.out.println("Move [" + i + "] is " + childValue);
+					}
+					
+					if (childValue > bestValue == isMax)
+					{
+						if (firstNode)
+						{
+							bestIndices.clear();
+							bestIndices.add(i);
+						}
+						bestValue = childValue;
+					}
+					else if (childValue == bestValue)
+					{
+						if (firstNode)
+						{
+							//bestIndices.add(i);
+						}
+					}
+					if (isMax == false && childValue < beta)
+					{
+						beta = childValue;
+					}
+					else if (isMax && childValue > alpha)
+					{
+						alpha = childValue;
+					}
+					
+					//node.updateValue(child.getValue());
+					
+					if (alpha >= beta && firstNode == false)
+					{
+						// Pruning
+						
+						if (firstNode == false)
+						{
+							bsp1[x][y][z] = false;
+						}
+						return isMax ? 10000.0 : -10000.0;
+					}
+				}
+			}
+			
+			if (firstNode == false)
+			{
+				bsp1[x][y][z] = false;
+			}
+			return bestValue;
+		}
+	}
+	
+	private double evaluateGame(long bp1, long bp2)
+	{
+		double res = 0.0;
+		
+		for (int i = 0; i < GameAnalyzer.longLines.length; i++)
+		{
+			int self = 0;
+			int other = 0;
+			
+			long p1Here = bp1 & GameAnalyzer.longLines[i];
+			long p2Here = bp2 & GameAnalyzer.longLines[i];
+			
+			while (p1Here != 0x0)
+			{
+				p1Here &= (p1Here - 1);
+				self++;
+			}
+			while (p2Here != 0x0)
+			{
+				p2Here &= (p2Here - 1);
+				other++;
+			}
+			
+			if (other == 0) {
+				switch (self) {
+				case 4:
+					return win_p;
+				case 3:
+					res += triple_p;
+					break;
+				case 2:
+					res += double_p;
+					break;
+				case 1:
+					res += single_p;
+					break;
+				default:
+					break;
+				}
+			}
+			if (self == 0) {
+				switch (other) {
+				case 4:
+					return -win_p;
+				case 3:
+					res -= triple_p;
+					break;
+				case 2:
+					res -= double_p;
+					break;
+				case 1:
+					res -= single_p;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		
+		return res;
+	}
+	
+	
+	private double evaluateGame(boolean[][][] b1s, boolean[][][] b2s)
+	{
+		double res = 0.0;
+		
+		for (int i = 0; i < GameAnalyzer.lines.length; i++)
+		{
+			int self = 0;
+			int other = 0;
+			
+			for (int j = 0; j < 4; j++)
+			{
+				self += GameAnalyzer.isAir(b1s, GameAnalyzer.lines[i][j]) ? 0 : (GameAnalyzer.isP1(b2s, GameAnalyzer.lines[i][j]) ? 1 : 0);
+				other += GameAnalyzer.isAir(b1s, GameAnalyzer.lines[i][j]) ? 0 : ((GameAnalyzer.isP1(b2s, GameAnalyzer.lines[i][j]) == false) ? 1 : 0);
+			}
+			
+			if (other == 0) {
+				switch (self) {
+				case 4:
+					return win_p;
+				case 3:
+					res += triple_p;
+					break;
+				case 2:
+					res += double_p;
+					break;
+				case 1:
+					res += single_p;
+					break;
+				default:
+					break;
+				}
+			}
+			if (self == 0) {
+				switch (other) {
+				case 4:
+					return -win_p;
+				case 3:
+					res -= triple_p;
+					break;
+				case 2:
+					res -= double_p;
+					break;
+				case 1:
+					res -= single_p;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		
+		return res;
+	}
+	
+	private double evaluateGame(MrMoreefficientGamestate game)
+	{
+		double val = evaluateGameG(game);
+		
+		/*SogoGame g = new SogoGame();
+		for (int x = 0; x < 4; x++)
+		{
+			for (int y = 0; y < 4; y++)
+			{
+				for (int z = 0; z < 4; z++)
+				{
+					if (game.fieldAir(x, y, z) == true)
+					{
+						g.board[x][y][z] = Player.NONE;
+					}
+					else
+					{
+						g.board[x][y][z] = game.fieldP(x, y, z, false) ? Player.P1 : Player.P2;
+					}
+				}
+			}
+		}
+		
+		double val = MrNovice.evaluateGame(g);*/
+		
+		return val;
+	}
+	
+	public static double evaluateGameG(MrMoreefficientGamestate g)
+	{
+		double res = 0.0;
+		
+		for (int i = 0; i < GameAnalyzer.lines.length; i++)
+		{
+			int self = 0;
+			int other = 0;
+			
+			for (int j = 0; j < 4; j++)
+			{
+				self += g.fieldAir(GameAnalyzer.lines[i][j]) ? 0 : (g.fieldP(GameAnalyzer.lines[i][j], false) ? 1 : 0);
+				other += g.fieldAir(GameAnalyzer.lines[i][j]) ? 0 : (g.fieldP(GameAnalyzer.lines[i][j], true) ? 1 : 0);
+			}
+			
+			if (other == 0) {
+				switch (self) {
+				case 4:
+					return win_p;
+				case 3:
+					res += triple_p;
+					break;
+				case 2:
+					res += double_p;
+					break;
+				case 1:
+					res += single_p;
+					break;
+				default:
+					break;
+				}
+			}
+			if (self == 0) {
+				switch (other) {
+				case 4:
+					return -win_p;
+				case 3:
+					res -= triple_p;
+					break;
+				case 2:
+					res -= double_p;
+					break;
+				case 1:
+					res -= single_p;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		
+		return res;
+	}
+}
--- Java Project/ConnectFour/src/util/GameAnalyzer.java
+++ Java Project/ConnectFour/src/util/GameAnalyzer.java
@@ -93,6 +93,97 @@ public class GameAnalyzer {
 		{{0,0,3},{1,1,2},{2,2,1},{3,3,0}},
 		{{3,0,0},{2,1,1},{1,2,2},{0,3,3}},
 		{{3,0,3},{2,1,2},{1,2,1},{0,3,0}},};
+		
+		public static final long[] longLines = new long[] {0x000000000000000F,
+				0x00000000000000F0,
+				0x0000000000000F00,
+				0x000000000000F000,
+				0x00000000000F0000,
+				0x0000000000F00000,
+				0x000000000F000000,
+				0x00000000F0000000,
+				0x0000000F00000000L,
+				0x000000F000000000L,
+				0x00000F0000000000L,
+				0x0000F00000000000L,
+				0x000F000000000000L,
+				0x00F0000000000000L,
+				0x0F00000000000000L,
+				0xF000000000000000L,
+
+				0x0000000000001111L,
+				0x0000000000002222L,
+				0x0000000000004444L,
+				0x0000000000008888L,
+				0x0000000011110000L,
+				0x0000000022220000L,
+				0x0000000044440000L,
+				0x0000000088880000L,
+				0x0000111100000000L,
+				0x0000222200000000L,
+				0x0000444400000000L,
+				0x0000888800000000L,
+				0x1111000000000000L,
+				0x2222000000000000L,
+				0x4444000000000000L,
+				0x8888000000000000L,
+
+				0x0001000100010001L,
+				0x0002000200020002L,
+				0x0004000400040004L,
+				0x0008000800080008L,
+				0x0010001000100010L,
+				0x0020002000200020L,
+				0x0040004000400040L,
+				0x0080008000800080L,
+				0x0100010001000100L,
+				0x0200020002000200L,
+				0x0400040004000400L,
+				0x0800080008000800L,
+				0x1000100010001000L,
+				0x2000200020002000L,
+				0x4000400040004000L,
+				0x8000800080008000L,
+				
+				
+				
+				
+
+				0x0008000400020001L,
+				0x0080004000200010L,
+				0x0800040002000100L,
+				0x8000400020001000L,
+				0x0001000200040008L,
+				0x0010002000400080L,
+				0x0100020004000800L,
+				0x1000200040008000L,
+
+				0x1000010000100001L,
+				0x2000020000200002L,
+				0x4000040000400004L,
+				0x8000080000800008L,
+				0x0001001001001000L,
+				0x0002002002002000L,
+				0x0004004004004000L,
+				0x0008008008008000L,
+
+				0x0000000000008421L,
+				0x0000000084210000L,
+				0x0000842100000000L,
+				0x8421000000000000L,
+				0x0000000000001248L,
+				0x0000000012480000L,
+				0x0000124800000000L,
+				0x1248000000000000L,
+				
+				
+				
+
+				0x8000040000200001L,
+				0x0001002004008000L,
+				0x1000020000400008L,
+				0x0008004002001000L,
+				};
 	
 	public static int countFreeLine(Player[] line, Player player) {
 		int c = 0;
@@ -108,6 +199,134 @@ public class GameAnalyzer {
 		return c;
 	}
 	
+	
+	public static boolean hasGameEnded(long bp1)
+	{
+		for (int i = 0; i < longLines.length; i++)
+		{
+			if ((bp1 & longLines[i]) != 0x0)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public static boolean[][][] getB1sFromGame(SogoGame g)
+	{
+		boolean[][][] b1s = new boolean[4][4][4];
+		
+		for (int x = 0; x < 4; x++)
+		{
+			for (int y = 0; y < 4; y++)
+			{
+				for (int z = 0; z < 4; z++)
+				{
+					if (g.board[x][y][z] == Player.NONE)
+					{
+						b1s[x][y][z] = false;
+					}
+					else
+					{
+						b1s[x][y][z] = true;
+					}
+				}
+			}
+		}
+		
+		return b1s;
+	}
+	
+	public static boolean[][][] getB2sFromGame(SogoGame g)
+	{
+		boolean[][][] b2s = new boolean[4][4][4];
+		
+		for (int x = 0; x < 4; x++)
+		{
+			for (int y = 0; y < 4; y++)
+			{
+				for (int z = 0; z < 4; z++)
+				{
+					if (g.board[x][y][z] == Player.NONE)
+					{
+
+					}
+					else
+					{
+						b2s[x][y][z] = g.board[x][y][z] != g.getCurrentPlayer();
+					}
+				}
+			}
+		}
+		
+		return b2s;
+	}
+	
+	public static boolean hasGameEnded(boolean[][][] b1s, boolean[][][] b2s, int x, int y, int z, boolean whoCouldHaveEndedIt)
+	{
+		boolean winX = true;
+		boolean winY = true;
+		boolean winZ = true;
+		boolean winCX1 = true;
+		boolean winCY1 = true;
+		boolean winCZ1 = true;
+		boolean winCX2 = true;
+		boolean winCY2 = true;
+		boolean winCZ2 = true;
+
+		boolean winQ1 = true;
+		boolean winQ2 = true;
+		boolean winQ3 = true;
+		boolean winQ4 = true;
+		for (int s = 0; s < 4; s++)
+		{
+			if (winX && (b1s[s][y][z] == false || b2s[s][y][z] != whoCouldHaveEndedIt))
+				winX = false;
+			if (winY && (b1s[x][s][z] == false || b2s[x][s][z] != whoCouldHaveEndedIt))
+				winY = false;
+			if (winZ && (b1s[x][y][s] == false || b2s[x][y][s] != whoCouldHaveEndedIt))
+				winZ = false;
+			
+
+			if (winCX1 && (b1s[x][s][s] == false || b2s[x][s][s] != whoCouldHaveEndedIt))
+				winCX1 = false;
+			if (winCY1 && (b1s[s][y][s] == false || b2s[s][y][s] != whoCouldHaveEndedIt))
+				winCY1 = false;
+			if (winCZ1 && (b1s[s][s][z] == false || b2s[s][s][z] != whoCouldHaveEndedIt))
+				winCZ1 = false;
+			if (winCX2 && (b1s[x][3-s][s] == false || b2s[x][3-s][s] != whoCouldHaveEndedIt))
+				winCX2 = false;
+			if (winCY2 && (b1s[3-s][y][s] == false || b2s[3-s][y][s] != whoCouldHaveEndedIt))
+				winCY2 = false;
+			if (winCZ2 && (b1s[3-s][s][z] == false || b2s[3-s][s][z] != whoCouldHaveEndedIt))
+				winCZ2 = false;
+			
+
+			if (winQ1 && (b1s[s][s][s] == false || b2s[s][s][s] != whoCouldHaveEndedIt))
+				winQ1 = false;
+			if (winQ2 && (b1s[s][s][3-s] == false || b2s[s][s][3-s] != whoCouldHaveEndedIt))
+				winQ2 = false;
+			if (winQ3 && (b1s[3-s][s][s] == false || b2s[3-s][s][s] != whoCouldHaveEndedIt))
+				winQ3 = false;
+			if (winQ4 && (b1s[3-s][s][3-s] == false || b2s[3-s][s][3-s] != whoCouldHaveEndedIt))
+				winQ4 = false;
+		}
+		
+		boolean hasEnded = winX || winY || winZ || winCX1 || winCX2 || winCY1 || winCY2 || winCZ1 || winCZ2 || winQ1 || winQ2 || winQ3 || winQ4;
+		
+		return hasEnded;
+	}
+	
+	public static boolean isAir(boolean[][][] b1s, int[] coords)
+	{
+		return b1s[coords[0]][coords[1]][coords[2]] == false;
+	}
+	
+	public static boolean isP1(boolean[][][] b2s, int[] coords)
+	{
+		return b2s[coords[0]][coords[1]][coords[2]] == false;
+	}
 	
 	
 	public static int getAmountOfLines(SogoGame g, int lineLength, Player player)
