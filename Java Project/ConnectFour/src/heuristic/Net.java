e7712df14a486a74d1a28f79da9db08f1d706aee
SimonBlasen <simon.yzf@t-online.de>
2017-12-29 01:24:25 +0100

    Started writing parser

--- Java Project/ConnectFour/src/heuristic/Net.java
+++ Java Project/ConnectFour/src/heuristic/Net.java
@@ -1,13 +1,79 @@
 package heuristic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Net {
 
 //struct fann_neuron *neuron_it, *first_neuron;
 
-	int mNumInput = 0;
-	Layer[] mLayers;
+	//private int mNumInput = 0;
+	private Layer[] mLayers;
 	
 	
+	// Parameters in struct fann
+	//
+	
+	private float learning_rate;
+	private float learning_momentum;
+	private float connection_rate;
+	private Nettype_Enum network_type;
+	private int total_neurons;
+	private int num_inputs;
+	private int num_outputs;
+	private float[] weights;
+	
+	
+	private Layer first_layer;
+	private Layer last_layer;
+	
+	//
+	//	
+	
+	
+	public Net(String configFile)
+	{
+		List<String> list = new ArrayList<>();
+
+		try (BufferedReader br = Files.newBufferedReader(Paths.get(configFile))) {
+
+			//br returns as stream and convert it into a List
+			list = br.lines().collect(Collectors.toList());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		for (int i = 0; i < list.size(); i++)
+		{
+			String decl = list.get(i).split("=").length == 2 ? list.get(i).split("=")[0] : "";
+			String val = list.get(i).split("=").length == 2 ? list.get(i).split("=")[1] : "";
+			
+			try
+			{
+				if (decl.equals("learning_rate"))
+				{
+					learning_rate = Float.valueOf(val);
+				}
+				
+				else if (decl.equals("learning_momentum"))
+				{
+					learning_momentum = Float.valueOf(val);
+				}
+			}
+			catch (Exception ex)
+			{
+				System.out.println("Error reading net config file at \"" + configFile + "\"");
+			}
+		}
+	}
 	
 	public static float run(Net net, int[] input) {
 		
@@ -31,7 +97,7 @@ public class Net {
 
 		float maxSum = 0;
 		
-		numInput = net.mNumInput;
+		numInput = net.num_inputs;
 		
 		//for fast acces:
 		Neuron firstNeuron = net.mLayers[0].getFirstNeuron();
---
+++ Java Project/ConnectFour/src/heuristic/Nettype_Enum.java
@@ -0,0 +1,6 @@
+package heuristic;
+
+public enum Nettype_Enum {
+	FANN_NETTYPE_LAYER, FANN_NETTYPE_SHORTCUT
+
+}
